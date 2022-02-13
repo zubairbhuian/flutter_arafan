df9fc496b27cbdcd375b56e9d154af6007282441
Zubair <zubairbhuian@gmail.com>
2022-02-13 21:50:01 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_test_one/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_test_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Doctor's App"),
+        ),
+        body: const Center(
+          child: Text("This is my app"),
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
