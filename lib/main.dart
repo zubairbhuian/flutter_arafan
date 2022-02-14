c26b9cfb142fa1169a38e6896d43cbe209f698c9
Zubair <zubairbhuian@gmail.com>
2022-02-14 21:47:40 +0600

    test14.3

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,62 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           // primaryColor:Colors.red,
-          primarySwatch: Colors.grey),
+          primarySwatch: Colors.orange),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("My App"),
+          title: const Text(
+            "My App",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children:[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                ),
+                child: Text(
+                  "Hi ! BHuian",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.message),
+                title: const Text("Message"),
+                onTap: (){}
+              ),
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text("Profile"),
+                onTap: (){}
+
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text("Settings"),
+                onTap: (){}
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text("Logout"),
+                onTap: (){}
+              )
+            ],
+          ),
         ),
         body: const Center(child: Text("This is my app")),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () {},
-          label: Text("More"),
-          icon: Icon(
+          label: const Text("More", style: TextStyle(color: Colors.white)),
+          icon: const Icon(
             Icons.more,
             color: Colors.white,
-            ),
+          ),
         ),
       ),
     );
