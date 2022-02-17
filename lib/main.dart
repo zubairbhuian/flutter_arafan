df1e882ec39eeaa67f16b037b72403475244fb92
Zubair <zubairbhuian@gmail.com>
2022-02-17 20:40:45 +0600

    terst17.6

--- lib/main.dart
+++ lib/main.dart
@@ -41,79 +41,14 @@ class MyApp extends StatelessWidget {
                 color: Colors.white),
           ],
         ),
-        drawer: Drawer(
-          backgroundColor: Colors.white,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                ),
-                child: Text(
-                  "Hi ! BHuian",
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-              ),
-              ListTile(
-                  leading: const Icon(Icons.message),
-                  title: const Text("Message"),
-                  onTap: () {}),
-              ListTile(
-                  leading: const Icon(Icons.person),
-                  title: const Text("Profile"),
-                  onTap: () {}),
-              ListTile(
-                  leading: const Icon(Icons.settings),
-                  title: const Text("Settings"),
-                  onTap: () {}),
-              ListTile(
-                  leading: const Icon(Icons.logout),
-                  title: const Text("Logout"),
-                  onTap: () {}),
-            ],
-          ),
-        ),
         body: const Center(
-          child:MysateLessWidget(),
         ),
-        floatingActionButton: FloatingActionButton.extended(
+        floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          label: const Text("More", style: TextStyle(color: Colors.white)),
-          icon: const Icon(
-            Icons.more,
-            color: Colors.white,
-          ),
+          child: const Icon(Icons.send,color: Colors.white,)
         ),
       ),
     );
   }
 }
 
-class MysateLessWidget extends StatelessWidget {
-  const MysateLessWidget({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text("Click",style: TextStyle(color: Colors.white,)),
-      onPressed: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: const Text('Awesome Snackbar!'),
-            action: SnackBarAction(
-              label: 'Action',
-              onPressed: () {
-                // Code to execute.
-              },
-            ),
-          ),
-        );
-      },
-      
-    );
-  }
-}
