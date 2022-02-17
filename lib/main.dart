da0dcba8176329c8c0b3dae95b86b026eb3cc45e
Zubair <zubairbhuian@gmail.com>
2022-02-17 15:14:38 +0600

    test17.4

--- lib/main.dart
+++ lib/main.dart
@@ -99,25 +99,16 @@ class MysateLessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      child: const Text("Click"),
+      child: const Text("Click",style: TextStyle(color: Colors.white,)),
       onPressed: () {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
+            content: const Text('Awesome Snackbar!'),
             action: SnackBarAction(
               label: 'Action',
               onPressed: () {
                 // Code to execute.
               },
-            ),
-            content: const Text('Awesome SnackBar!'),
-            duration: const Duration(milliseconds: 1500),
-            width: 280.0, // Width of the SnackBar.
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8.0, // Inner padding for SnackBar content.
-            ),
-            behavior: SnackBarBehavior.floating,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
             ),
           ),
         );
