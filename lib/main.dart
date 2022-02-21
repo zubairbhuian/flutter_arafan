96eedefa806226b237f7655f718077358570b23a
Zubair <zubairbhuian@gmail.com>
2022-02-21 15:35:05 +0600

    21.6

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: HomePage(),
     );
   }
 }
 
-
-
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -27,25 +25,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo),
-            label:Text("dsfsd"),
-            ),
-                      BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo),
-            ),
-                      BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo),
-            ),
-                      BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo),
-            ),
-        ],
+      appBar: AppBar(
+        title: Text("fsdf"),
       ),
-      body: Center(),
-      
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      ]),
     );
   }
 }
