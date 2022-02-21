5721139844773be1a773b67d0239e0cfb179c125
Zubair <zubairbhuian@gmail.com>
2022-02-21 10:08:52 +0600

    21.3

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,30 @@ import 'package:matcher/matcher.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text('Hi'),)),
-      
+      home: Scaffold(
+          body: PageView(
+            controller:_controller ,
+            children: [
+              Page1(),
+              Page2(),
+              Page3(),
+            ],
+
+      )),
     );
   }
 }
