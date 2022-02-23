45529ab1c29223ea2bdc4a33bcc33e97d35b12a8
Zubair <zubairbhuian@gmail.com>
2022-02-23 19:08:07 +0600

    23.4

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
-
-
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,24 +13,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int value = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        
+        body: Column(children: [
+          Text(value.toString(),style: TextStyle(fontSize: 50),)
+        ]),
       ),
-      
     );
   }
 }
