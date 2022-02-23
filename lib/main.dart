19b569ad5ca862cace218f1150d9d4f2c9e879a0
Zubair <zubairbhuian@gmail.com>
2022-02-23 19:00:05 +0600

    23.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
+
+
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
-      ),
+      home: HomePage(),
     );
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      alignment: Alignment.center,
-      width: 100.0,
-      height: 100.0,
-      child: AspectRatio(
-        aspectRatio: 2.0,
-        child: Container(
-          width: 100.0,
-          height: 50.0,
-          color: Colors.green,
-        ),
+    return SafeArea(
+      child: Scaffold(
+        
       ),
+      
     );
   }
 }
