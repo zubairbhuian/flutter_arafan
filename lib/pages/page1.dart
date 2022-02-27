2cf2f35bc68f28f4c4cfc9b8f27cc93ad8595365
Zubair <zubairbhuian@gmail.com>
2022-02-27 14:11:45 +0600

    27.5

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,15 +10,37 @@ class Page1 extends StatefulWidget {
   _Page1State createState() => _Page1State();
 }
 
-Future getData() async {
-  var responts =
-      await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
-      
-}
-
 class _Page1State extends State<Page1> {
+  var data = [];
+  Future getData() async {
+    var response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+    setState(() {
+      var decode = jsonDecode(response.body);
+      data = decode;
+      print(data);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (contex, index) {
+          return ListTile(
+            title: Text(data[index]["title"]),
+            subtitle: Text(data[index]["body"]),
+          );
+        },
+      ),
+    );
   }
 }
