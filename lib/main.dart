832cd1fd63937080d4e7a3ee6a1e27058faedeaa
Zubair <zubairbhuian@gmail.com>
2022-02-28 10:47:23 +0600

    28.2

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.amber),
-      home: const Scaffold(
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: Scaffold(
           body: Center(
-        child: Text("hi bhuian"),
-      )),
+              child: ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            child: Container(
+                              width: 200,
+                              height: 200,
+                              color: Colors.green,
+                              child: Column(children: const[
+                                Text("data"),
+                                SizedBox(height: 20),
+                                Text("Here is some demo text"),
+                              ]),
+                            ),
+                          );
+                        });
+                  },
+                  child: const Text("Click Me")))),
     );
   }
 }
