b37636adf03ebd3ce8c7165c7ba9950fa958192b
Zubair <zubairbhuian@gmail.com>
2022-02-28 15:11:20 +0600

    28.3

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.pink),
-      home: Scaffold(
-          body: Center(
-              child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return Dialog(
-                            child: Container(
-                              width: 200,
-                              height: 200,
-                              color: Colors.green,
-                              child: Column(children: const[
-                                Text("data"),
-                                SizedBox(height: 20),
-                                Text("Here is some demo text"),
-                              ]),
-                            ),
-                          );
-                        });
-                  },
-                  child: const Text("Click Me")))),
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: Scaffold(body: MyPopup()),
+    );
+  }
+}
+
+class MyPopup extends StatefulWidget {
+  const MyPopup({Key? key}) : super(key: key);
+
+  @override
+  _MyPopupState createState() => _MyPopupState();
+}
+
+var btn = "Click Me";
+
+class _MyPopupState extends State<MyPopup> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (
+                builder,
+              ) {
+                return Dialog(
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(children: [
+                        Text("Hi Bhuian"),
+                        Text("Here is some demo text")
+                      ]),
+                    ),
+                  ),
+                );
+              });
+        },
+        child: Text(btn),
+      ),
     );
   }
 }
