694288a93e2b499545e79ff206cfc475f629185b
Zubair <zubairbhuian@gmail.com>
2022-03-01 19:33:25 +0600

    1.2

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => {runApp(MyApp())};
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: SafeArea(child: Scaffold(appBar:AppBar(title: const Text("My App"),actions: [
+        ElevatedButton(onPressed: (){}, child: const Text("Log In"))
+      ],) ),),      
+    );
+  }
+}
+
+
+
