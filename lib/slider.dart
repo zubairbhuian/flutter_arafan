cc52a1bb5f451a9424f1b2bc3086058af9f8d4d8
Zubair <zubairbhuian@gmail.com>
2022-03-01 19:47:37 +0600

    1.5

--- lib/slider.dart
+++ lib/slider.dart
@@ -11,7 +11,10 @@ class _SliderDemoState extends State<SliderDemo> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(onPressed: (){},child:const Text('Click Me')),
+     child:Column(children: [
+       ElevatedButton(onPressed: (){},child:const Text('Click Me')),
+       ElevatedButton(onPressed: (){},child:const Text('Hide Me')),
+     ],),
       
     );
   }
