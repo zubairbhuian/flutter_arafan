18718b820bd7cfbd05174faac0cf7f1c8249c839
Zubair <zubairbhuian@gmail.com>
2022-03-02 16:05:29 +0600

    dropdown.1

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
-# RichText  in flutter
+# dropdown button  in flutter
 
 ## Copy the code
 ```dart
 
-Slider(
-  min: 16,
-  max: 40,
-  value: _value,
-  onChanged: (value) {
-  setState(() {
-    _value = value;
-  });
-})
+  DropdownButton(
+      items: const [
+        DropdownMenuItem(value: "Home", child: Text("Home")),
+        DropdownMenuItem(value: "About", child: Text("About")),
+        DropdownMenuItem(
+            value: "Contact Us", child: Text("Contact Us")),
+        DropdownMenuItem(value: "Hire Me", child: Text("Hire Me")),
+      ],
+      onChanged: (val) {
+        setState(() {
+          _selected = val.toString();
+        });
+      })
 
 ```
 
 
 ## ScreenShort
-![](imgs/Screenshot_2.png)
+![](imgs/Screenshot_4.png)
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _selected = "Chose";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,17 +18,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.green),
       home: SafeArea(
           child: Scaffold(
+        backgroundColor: Colors.grey,
         body: Center(
-          child: RichText(
-              text: const TextSpan(
-                  text: "Hello,how are you, ",
-                  style: TextStyle(color: Colors.red),
-                  children: [
-                TextSpan(
-                    text: "Zubair?",
-                    style: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.bold)),
-              ])),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                _selected,
+                style: const TextStyle(fontSize: 22),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DropdownButton(
+                  items: const [
+                    DropdownMenuItem(value: "Home", child: Text("Home")),
+                    DropdownMenuItem(value: "About", child: Text("About")),
+                    DropdownMenuItem(
+                        value: "Contact Us", child: Text("Contact Us")),
+                    DropdownMenuItem(value: "Hire Me", child: Text("Hire Me")),
+                  ],
+                  onChanged: (val) {
+                    setState(() {
+                      _selected = val.toString();
+                    });
+                  })
+            ],
+          ),
         ),
       )),
     );
