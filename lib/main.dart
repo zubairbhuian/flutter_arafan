6fc04c56726a1a8204b457a13ebf5b9e9283714c
Zubair <zubairbhuian@gmail.com>
2022-03-02 19:21:15 +0600

    2.6

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,84 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _selected = "Chose";
+  bool checked = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: SafeArea(
-          child: Scaffold(
-        backgroundColor: Colors.grey,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                _selected,
-                style: const TextStyle(fontSize: 22),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              DropdownButton(
-                  items: const [
-                    DropdownMenuItem(value: "Home", child: Text("Home")),
-                    DropdownMenuItem(value: "About", child: Text("About")),
-                    DropdownMenuItem(
-                        value: "Contact Us", child: Text("Contact Us")),
-                    DropdownMenuItem(value: "Hire Me", child: Text("Hire Me")),
-                  ],
-                  onChanged: (val) {
-                    setState(() {
-                      _selected = val.toString();
-                    });
-                  })
-            ],
-          ),
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: AnimatedCrossFade(
+                  firstChild: Container(
+                    height: 200,
+                    color: Colors.purple,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          
+                        });
+                      },
+                      child: Text("Click Me"),
+                    ),
+                  ),
+                  secondChild: Center(
+                    child: Container(
+                      child: const Text(
+                        "Well to my App",
+                        style: TextStyle(fontSize: 32),
+                      ),
+                    ),
+                  ),
+                  crossFadeState: checked
+                      ? CrossFadeState.showFirst
+                      : CrossFadeState.showSecond,
+                  duration: Duration(seconds: 1)),
+            )
+          ],
         ),
-      )),
+      ),
+    );
+  }
+}
+
+// first child
+class MyFirstChild extends StatefulWidget {
+  const MyFirstChild({Key? key}) : super(key: key);
+
+  @override
+  State<MyFirstChild> createState() => _MyFirstChildState();
+}
+
+class _MyFirstChildState extends State<MyFirstChild> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {});
+        },
+        child: Text("Click Me"),
+      ),
+    );
+  }
+}
+
+// second child
+class MySecoundChild extends StatelessWidget {
+  const MySecoundChild({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: const Text(
+          "Well to my App",
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
     );
   }
 }
