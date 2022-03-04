4b9f9d6fc7dca39be342719966ee36029cf24653
Zubair <zubairbhuian@gmail.com>
2022-03-04 08:33:03 +0600

    4.2

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: const SafeArea(child: SecondScreen()),
+    );
+  }
+}
+
+class FristScreen extends StatefulWidget {
+  const FristScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FristScreen> createState() => _FristScreenState();
+}
+
+class _FristScreenState extends State<FristScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: OutlinedButton(onPressed: () {}, child: Text("First Screen")),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: OutlinedButton(onPressed: () {}, child: Text("Second Screen")),
+      ),
+    );
+  }
+}
